244442bd24ddb4ee32e4ca3b6182233f93e36851
arqamtech <hi@arqam.tech>
2019-03-22 02:11:30 +0530

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD1NP1tsMMqXPKNisSXaOQQCadfx5ENvPY",
+    authDomain: "cms-codebro.firebaseapp.com",
+    databaseURL: "https://cms-codebro.firebaseio.com",
+    projectId: "cms-codebro",
+    storageBucket: "cms-codebro.appspot.com",
+    messagingSenderId: "918796601903"
+  }
+};
